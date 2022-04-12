73c5b3ef061405ee66f570a2ef413029ccb8a583
rafiul <bmrafiul.alam@gamail.com>
2022-04-12 17:17:20 +0600

    Config Link and Pro link added

--- admin/plugin_setting_link.php
+++ admin/plugin_setting_link.php
@@ -6,7 +6,8 @@
  * @since v1.0
  */
 function wactc_add_action_links( $links ) {
-    $wactc_links[] = '<a href="' . esc_url( admin_url('admin.php?page=wactc-add-to-cart') ) . '" title="' . esc_attr( __( 'Add to Cart Setting Page', 'wactc' ) ) . '">' . esc_html( __( 'Configure', 'wactc' ) ) . '</a>';
+    $wactc_links[] = '<a href="' . esc_url( admin_url('admin.php?page=wactc_button_text') ) . '" title="' . esc_attr( __( 'Add to Cart Setting Page', 'wactc' ) ) . '">' . esc_html( __( 'Configure', 'wactc' ) ) . '</a> | <a style="color:green" href="https://codeastrology.com/downloads/add-to-cart-text-changer-and-customise-button-premium/" title="' . esc_attr( __( 'Go to Premium', 'wactc' ) ) . '">' . esc_html( __( 'Go Premium', 'wactc' ) ) . '</a>';
+    
     return array_merge( $wactc_links, $links );
 }
 add_filter( 'plugin_action_links_' . WACTC_PLUGIN_BASE_FILE, 'wactc_add_action_links' );
