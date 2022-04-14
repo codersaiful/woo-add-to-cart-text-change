23ed9bc93352d7f4358638269b6050ce553e58ce
Saiful Islam <codersaiful@gmail.com>
2022-04-14 10:46:09 +0600

    link updated

--- admin/plugin_setting_link.php
+++ admin/plugin_setting_link.php
@@ -6,7 +6,7 @@
  * @since v1.0
  */
 function wactc_add_action_links( $links ) {
-    $wactc_links[] = '<a href="' . esc_url( admin_url('admin.php?page=wactc_button_text') ) . '" title="' . esc_attr( __( 'Add to Cart Setting Page', 'wactc' ) ) . '">' . esc_html( __( 'Configure', 'wactc' ) ) . '</a> | <a style="color:green" href="https://codeastrology.com/downloads/add-to-cart-text-changer-and-customise-button-premium/" title="' . esc_attr( __( 'Go to Premium', 'wactc' ) ) . '">' . esc_html( __( 'Go Premium', 'wactc' ) ) . '</a>';
+    $wactc_links[] = '<a href="' . esc_url( admin_url('admin.php?page=wactc_button_text') ) . '" title="' . esc_attr( __( 'Add to Cart Setting Page', 'wactc' ) ) . '">' . esc_html( __( 'Configure', 'wactc' ) ) . '</a> | <a style="color:green" href="https://codeastrology.com/pricing-add-to-cart-button-changer/" title="' . esc_attr( __( 'Go to Premium', 'wactc' ) ) . '">' . esc_html( __( 'Go Premium', 'wactc' ) ) . '</a>';
     
     return array_merge( $wactc_links, $links );
 }
